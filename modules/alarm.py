d05834e0b780a0334a72458575eae8160b8f81d3
Mike Goodman <followthroughworks@gmail.com>
2015-06-11 15:51:32 -0400

    Updated alarm to take duration and filename as arguments

--- modules/alarm.py
+++ modules/alarm.py
@@ -2,12 +2,10 @@ from pydub import AudioSegment
 from pydub import playback
 import time
 
-SONG_PATH = 'You Are All I See.mp3'
-
-def play_song():
+def play_song(duration_secs,song_path='music/bugle.mp3'):
     print("Retrieving alarm...")
-    song = AudioSegment.from_mp3(SONG_PATH)
-    song = song[:10000] #first ten seconds
+    song = AudioSegment.from_mp3(song_path)
+    song = song[:(duration_secs*1000)] #first ten seconds
     song_duration = len(song)
     playback.play(song)
     #time.sleep(song_duration/1000)
